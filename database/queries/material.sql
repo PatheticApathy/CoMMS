2290713917d467e727cebb74db79835627fbbc6a
Gunth15 <cjw041@email.latech.edu>
2025-01-08 08:27:28 -0600

    Added more queries

---
+++ database/queries/material.sql
@@ -0,0 +1,8 @@
+-- name: GetAllMaterials :many
+SELECT id, name, type, quantity, unit, status, location_lat, location_lng, job_site_id, last_checked_out FROM Materials;
+
+-- name: GetMaterialsByType :many
+SELECT id, name, type, quantity, unit, status, location_lat, location_lng, job_site_id, last_checked_out FROM Materials WHERE type=?;
+
+-- name: GetMaterialsBySite :many
+SELECT id, name, type, quantity, unit, status, location_lat, location_lng, job_site_id, last_checked_out FROM Materials WHERE job_site=?;
--- database/queries/user.sql
+++ database/queries/user.sql
@@ -1,2 +1,8 @@
 -- name: GetAllUsers :many
-SELECT * FROM Users;
+SELECT id, username, site_id, role FROM Users;
+
+-- name: GetUser :one
+SELECT id, username, site_id, role FROM Users WHERE id = ?;
+
+-- name: AddUser :one
+INSERT INTO Users(id, username, site_id, role) VALUES (?,?,?,?) RETURNING *;
