8c1fb5cd5a26dd3a37d74f28c60a2ab9fd40bcca
Gunth15 <cjw041@email.latech.edu>
2025-01-09 08:44:19 -0600

    Adding queries I guesss NOTE: delete materials.db

--- database/queries/material.sql
+++ database/queries/material.sql
@@ -6,3 +6,9 @@ SELECT id, name, type, quantity, unit, status, location_lat, location_lng, job_s
 
 -- name: GetMaterialsBySite :many
 SELECT id, name, type, quantity, unit, status, location_lat, location_lng, job_site, last_checked_out FROM Materials WHERE job_site=?;
+
+-- name: GetMaterialsByQuantity :many
+SELECT id, name, type, quantity, unit, status, location_lat, location_lng, job_site, last_checked_out FROM Materials WHERE quantity=? AND unit=?;
+
+-- name: AddMaterial :one
+INSERT INTO Materials(id,name,type,quantity,unit,status,location_lat,location_lng,job_site) VALUES (?,?,?,?,?,?,?,?,?) RETURNING *; 
