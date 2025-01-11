a581f7385b5c7fd7f4ca3601d8b089bf60968d47
PatheticApathy <jasonleblanc03@icloud.com>
2025-01-11 12:58:01 -0600

    UserDB

---
+++ Userdb/migrations/20250111163250_permissions.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE Permissions (
+  permission INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  descripion TEXT NOT NULL,
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE Permissions;
+-- +goose StatementEnd
