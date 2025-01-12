2ab211ad1cf7f2c772c15f480f9db8c0cd497d26
Cameron White <113713364+Gunth15@users.noreply.github.com>
2025-01-12 16:29:38 -0600

    Update setup.sh

--- street_maps/setup.sh
+++ street_maps/setup.sh
@@ -3,7 +3,7 @@
 
 #Create volume install container and import volumes
 docker volume create osm-data
-docker run -v ./osm_data/Louisiana.osm.pbf:/data/region.osm.pbf -v osm-data:/data/database/ overv/openstreetmap-tile-server import
+docker run -v ./Louisiana.osm.pbf:/data/region.osm.pbf -v osm-data:/data/database/ overv/openstreetmap-tile-server import
 
 ##copy to systemd to run on boot
 cp ./map.service /etc/systemd/system/
