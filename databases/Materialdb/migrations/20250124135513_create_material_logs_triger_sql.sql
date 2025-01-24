0a01bf7e34be1441750db929db8be2f5fcba600e
Gunth15 <cjw041@email.latech.edu>
2025-01-24 13:24:52 -0600

    Added triggers to create logs and changes status

---
+++ databases/Materialdb/migrations/20250124135513_create_material_logs_triger_sql.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TRIGGER material_creation_log AFTER INSERT ON Materials
+BEGIN
+  INSERT 
+  INTO MaterialLogs(material_id, note, status, quantity_change,timestamp)
+  VALUES (new.id,"Created",new.status,new.quantity,date()); 
+END;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TRIGGER material_creation_log;
+-- +goose StatementEnd
---
+++ databases/Materialdb/migrations/20250124145917_update_material_log_trigger_sql.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TRIGGER material_update_log AFTER UPDATE ON Materials
+WHEN (old.quantity != new.quantity)
+BEGIN
+    INSERT 
+    INTO MaterialLogs(material_id, note, status, quantity_change,timestamp)
+    VALUES (new.id,"Quantity change",new.status,new.quantity,date());
+END;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TRIGGER material_update_log
+-- +goose StatementEnd
