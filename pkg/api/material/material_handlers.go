992581f73d2897ad8134f8cbed35dd9627cf016d
Gunth15 <cjw041@email.latech.edu>
2025-01-27 08:12:06 -0600

    Added docs to and more logging to material api

---
+++ pkg/api/material/material_handlers.go
@@ -0,0 +1,246 @@
+package material
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	materialdb "github.com/PatheticApathy/CoMMS/pkg/databases/materialdb"
+)
+
+// getMaterialHandler is a handler that returns materials based on given parameters godoc
+//
+//	@Summary		fetches material based on given  query parameters
+//	@Description	Can get material using its id, quantity and unit, type, or job site
+//	@Tags			material
+//	@Produce		json
+//	@Param			id			query		int						false	"id of material"
+//	@Param			quantity	query		int						false	"quantity of material, must be combined with unit"
+//	@Param			unit		query		string					false	"unit of the quantity, must be combined with quantity"
+//	@Param			type		query		string					false	"type of material"
+//	@Param			site		query		int						false	"site id of which material belongs to"
+//	@Success		200			{array}	materialdb.Material	"material"
+//	@Failure		400			{string} string	"bad request"
+//	@Failure		500			{string} string 	"Internal Server Error"
+//	@Router			/material/search [get]
+func (e *Env) getMaterialHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+
+	// has param id
+	if query.Has("id") {
+		material_id, err := strconv.Atoi(query.Get("id"))
+		if err != nil {
+			log.Printf("Material id is invalid, reason %e", err)
+			http.Error(w, "invalid material id", http.StatusBadRequest)
+			return
+		}
+
+		materials, err := e.Queries.GetMaterialsByID(r.Context(), int64(material_id))
+		if err != nil {
+			log.Printf("Material id is invalid, reason %e", err)
+			http.Error(w, "invalid material id", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Material %d found", material_id)
+		if err := json.NewEncoder(w).Encode(&materials); err != nil {
+			log.Printf("Could not encode to json %e", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+
+	// has param quantity and unit
+	if query.Has("quantity") && r.URL.Query().Has("unit") {
+
+		quantity, err := strconv.Atoi(query.Get("quantity"))
+		if err != nil {
+			log.Printf("Quantity is invalid, reason %e", err)
+			http.Error(w, "invalid quantity", http.StatusBadRequest)
+			return
+		}
+		unit := query.Get("unit")
+		args := materialdb.GetMaterialsByQuantityParams{
+			Quantity: int64(quantity),
+			Unit:     unit,
+		}
+
+		materials, err := e.Queries.GetMaterialsByQuantity(r.Context(), args)
+		if err != nil {
+			log.Printf("Quantity or unit is invalid, reason %e", err)
+			http.Error(w, "invalid quantity or unit", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Material(s) found with quantity %d%s", quantity, unit)
+		if err := json.NewEncoder(w).Encode(&materials); err != nil {
+			log.Printf("Could not create json: %e", err)
+			http.Error(w, "invalid quantity or unit", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// has param site
+	if query.Has("site") {
+		site, err := strconv.Atoi(query.Get("site"))
+		if err != nil {
+			log.Printf("Invalid site id, reason %e", err)
+			http.Error(w, "invalid id", http.StatusBadRequest)
+			return
+		}
+
+		args := sql.NullInt64{
+			Int64: int64(site),
+			Valid: true,
+		}
+
+		materials, err := e.Queries.GetMaterialsBySite(r.Context(), args)
+		if err != nil {
+			log.Printf("Invalid site id, reason %e", err)
+			http.Error(w, "invalid id", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Material(s) at site %d", site)
+		if err := json.NewEncoder(w).Encode(&materials); err != nil {
+			log.Printf("Could not create json: %e", err)
+			http.Error(w, "Error creating json", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// has param type
+	if query.Has("type") {
+
+		typ := query.Get("type")
+		args := sql.NullString{
+			String: query.Get("type"),
+			Valid:  true,
+		}
+
+		materials, err := e.Queries.GetMaterialsByType(r.Context(), args)
+		if err != nil {
+			log.Printf("Invalid type, reason %e", err)
+			http.Error(w, "invalid id", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Found materials of type %s", typ)
+		if err := json.NewEncoder(w).Encode(&materials); err != nil {
+			log.Printf("Could not create json: %e", err)
+			http.Error(w, "Error creating json", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// has no valid params
+	log.Println("Invalid query")
+	http.Error(w, "invalid query", http.StatusBadRequest)
+}
+
+// getAllMaterial is a handler that gets material from the database godoc
+//
+//	@Summary		gets all materials from database
+//	@Description	Gets all material form database
+//	@Tags			material
+//	@Produce		json
+//	@Success		200	{array}	materialdb.Material	"material"
+//	@Failure		500	{string} string	"Internal Server Error"
+//	@Router			/material/all [get]
+func (e *Env) getAllMaterial(w http.ResponseWriter, r *http.Request) {
+	materials, err := e.Queries.GetAllMaterials(r.Context())
+	if err != nil {
+		log.Printf(`Invalid Material insertion, reason %e`, err)
+		http.Error(w, `Internal Server Error`, http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Fetched all Material")
+	if err := json.NewEncoder(w).Encode(materials); err != nil {
+		log.Printf("Could not create json obj: %e", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+// postMaterialHandler is a handler that adds materials to the database godoc
+//
+//	@Summary		post materials to database
+//	@Description	Adds materials to the database using valid json structure
+//	@Tags			material
+//	@Produce		json
+//	@Accepts		json
+//	@Param			material	body		materialdb.AddMaterialParams	true	"Format of add material request"
+//	@Success		200			{object}	materialdb.Material				"material"
+//	@Failure		400			{string}	string "bad request"
+//	@Failure		500			{string}	string "Internal Server Error"
+//	@Router			/material/add [post]
+func (e *Env) postMaterialHandler(w http.ResponseWriter, r *http.Request) {
+	var material materialdb.AddMaterialParams
+
+	if err := json.NewDecoder(r.Body).Decode(&material); err != nil {
+		log.Printf("Could not parse json obj: %e", err)
+		http.Error(w, "Error parsing json", http.StatusBadRequest)
+		return
+	}
+
+	ret, err := e.Queries.AddMaterial(r.Context(), material)
+	if err != nil {
+		log.Printf(`Invalid Material insertion, reason %e`, err)
+		http.Error(w, `Could not add material`, http.StatusBadRequest)
+		return
+	}
+	log.Printf("Material with id %d added", ret.ID)
+
+	if err := json.NewEncoder(w).Encode(ret); err != nil {
+		log.Printf("Could not create json obj: %e", err)
+		http.Error(w, "Error creating json", http.StatusInternalServerError)
+		return
+	}
+}
+
+// changeMaterialQuantity is a handler that changes the quantity of the material and also the status if certain conditions are met godoc
+//
+//	@Summary		adjust quantity based on the requested amount
+//	@Description	changes the quantity of the given material using it's id
+//	@Tags			material
+//	@Produce		json
+//	@Accepts		json
+//	@Param			material	body		materialdb.ChangeQuantityParams	true	"Format of changing material quantity"
+//	@Success		200			{object}	materialdb.Material				"material"
+//	@Failure		400			{string} string	"bad request"
+//	@Failure		500			{string}	string "Internal Server Error"
+//	@Router			/material/change [put]
+func (e *Env) changeMaterialQuantity(w http.ResponseWriter, r *http.Request) {
+	var args materialdb.ChangeQuantityParams
+	if err := json.NewDecoder(r.Body).Decode(&args); err != nil {
+		log.Printf("Could not parse json obj: %e", err)
+		http.Error(w, "Error parsing json", http.StatusBadRequest)
+		return
+	}
+
+	material, err := e.Queries.ChangeQuantity(r.Context(), args)
+	if err != nil {
+		log.Printf(`Invalid Material change, reason %e`, err)
+		http.Error(w, `Could not change material quantity`, http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Changed quantity of material %s to %d", material.Name.String, args.Quantity)
+	if material.Quantity < 30 && material.Quantity <= 0 {
+		log.Printf(`Low material count for %v, changing status`, material.Name)
+	}
+
+	if material.Quantity <= 0 {
+		log.Printf(`Out of material %v, changing status changed`, material.Name)
+	}
+
+	if err := json.NewEncoder(w).Encode(&material); err != nil {
+		log.Printf("Could not create json obj: %e", err)
+		http.Error(w, "Error creating json", http.StatusInternalServerError)
+		return
+	}
+}
---
+++ pkg/api/material/material_log_handlers.go
@@ -0,0 +1,172 @@
+package material
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/PatheticApathy/CoMMS/pkg/databases/materialdb"
+)
+
+// getMaterialLogsHandler gets material log based on given params godoc
+//
+//	@Summary		fetches material logs based on given query parameters
+//	@Description	Can get material using it's id, or the material it relates to
+//	@Tags			material logs
+//	@Produce		json
+//	@Param			id			query		int						false	"id of material log"
+//	@Param			material	query		int						false	"id of material"
+//	@Success		200			{array}	materialdb.MaterialLog	"material log"
+//	@Failure		400			{string} string	"bad request"
+//	@Failure		500			{string} string	"Internal Server Error"
+//	@Router			/mlogs/search [get]
+func (e *Env) getMaterialLogsHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+
+	if query.Has("id") {
+
+		id, err := strconv.Atoi(query.Get("id"))
+		if err != nil {
+			log.Printf("Invalid material log id, reason %e", err)
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		material_log, err := e.Queries.GetMaterialLogsByID(r.Context(), int64(id))
+		if err != nil {
+			log.Printf("Invalid material log id, reason %e", err)
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Found material log %d", id)
+
+		if err := json.NewEncoder(w).Encode(&material_log); err != nil {
+			log.Printf("could not encode to json, reason %e", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+	if query.Has("material") {
+		id, err := strconv.Atoi(query.Get("material"))
+		if err != nil {
+			log.Printf("Invalid material id, reason %e", err)
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		material_log, err := e.Queries.GetMaterialLogsByMaterial(r.Context(), int64(id))
+		if err != nil {
+			log.Printf("Invalid material id, reason %e", err)
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Found material logs for material id %d", id)
+		if err := json.NewEncoder(w).Encode(&material_log); err != nil {
+			log.Printf("could not encode to json, reason %e", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+	log.Printf("No valid paramerters given")
+	http.Error(w, "bad request", http.StatusBadRequest)
+}
+
+// getAllMaterialLogsHandler gets all material logs
+//
+//	@Summary		fetches all material logs
+//	@Description	gets all material logs if they exist
+//	@Tags			material logs
+//	@Produce		json
+//	@Success		200	{array}	materialdb.MaterialLog	"material logs"
+//	@Failure		500	{string} string	"Internal Server Error"
+//	@Router			/mlogs/all [get]
+func (e *Env) getAllMaterialLogsHandler(w http.ResponseWriter, r *http.Request) {
+	logs, err := e.Queries.GetAllMaterialLogs(r.Context())
+	if err != nil {
+		log.Printf("No material logs, reason %e", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	log.Println("Fetching all material logs")
+
+	if err = json.NewEncoder(w).Encode(&logs); err != nil {
+		log.Printf("could not encode to json, reason %e", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+// postMaterialLogsHandler adds material logs godoc
+//
+//	@Summary		post material log to database
+//	@Description	Adds material log  to the database using valid json structure
+//	@Tags			material logs
+//	@Accept			json
+//	@Produce		json
+//	@Param			materiallog	body		materialdb.AddMaterialLogParams	true	"Format of add material log request"
+//	@Success		200			{array}	materialdb.MaterialLog		"material log"
+//	@Failure		400			{string} string	"bad request"
+//	@Failure		500			{string}	string "Internal Server Error"
+//	@Router			/mlogs/add [post]
+func (e *Env) postMaterialLogsHandler(w http.ResponseWriter, r *http.Request) {
+	var args materialdb.AddMaterialLogParams
+	if err := json.NewDecoder(r.Body).Decode(&args); err != nil {
+		log.Printf("could not decode to json, reason %e", err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	lg, err := e.Queries.AddMaterialLog(r.Context(), args)
+	if err != nil {
+		log.Printf("could add material log, reason %e", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Posted new material log with id %d", lg.ID)
+	if err = json.NewEncoder(w).Encode(&lg); err != nil {
+		log.Printf("could not encode to json, reason %e", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+// changeMaterialLogNoteHandler changes material log note godoc
+//
+//	@Summary		post changes not on a existing material log
+//	@Description	changes the not field of a materiallog
+//	@Tags			material logs
+//	@Accept			json
+//	@Produce		json
+//	@Param			note_and-id	body		materialdb.ChangeMaterialNoteParams	true	"Format of changing matterial note request"
+//	@Success		200			{object}	materialdb.MaterialLog				"material log"
+//	@Failure		400			{string} string	"bad request"
+//	@Failure		500			{string} string	"Internal Server Error"
+//	@Router			/mlogs/note [put]
+func (e *Env) changeMaterialLogNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var args materialdb.ChangeMaterialNoteParams
+	if err := json.NewDecoder(r.Body).Decode(&args); err != nil {
+		log.Printf("could not decode json, reason %e", err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	lg, err := e.Queries.ChangeMaterialNote(r.Context(), args)
+	if err != nil {
+		log.Printf("Could change note attached to material, reason %e", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Changed note of material log with id %d", lg.ID)
+	if err = json.NewEncoder(w).Encode(&lg); err != nil {
+		log.Printf("json error, reason %e", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
