3ae81e008c4fb12e1118fc63a41fbbbb41e12206
Cheoddox <leeroywycoff@gmail.com>
2025-01-31 14:28:39 -0600

    Updated Login and Signup pages to only allow login and signup if information fields have required information

--- CoMMSFrontend/src/app/login/page.tsx
+++ CoMMSFrontend/src/app/login/page.tsx
@@ -1,21 +1,39 @@
+'use client'
+
 import Link from "next/link";
 import styles from "./page.module.css";
+import React, { useState } from 'react';
 
-export default function Jobs() {
+export default function Login() {
+
+  const [inputValue1, setInputValue1] = useState('');
+  
+  const handleChange1 = (event) => {
+    setInputValue1(event.target.value);
+  };
+  
+  const [inputValue2, setInputValue2] = useState('');
+  
+  const handleChange2 = (event) => {
+    setInputValue2(event.target.value);
+  };
+
+  const buttonEnable = inputValue1.length > 0 && inputValue2.length > 0;
+
   return (
     <main className={styles.main}>
         <div className={styles.headerDivLogin1}>Login
         </div>
         <div className={styles.bodyDivLogin1}>
             <div className={styles.bodyDivLogin2}>
-                <input type="username" placeholder="Username" className={styles.inputLogin}></input>
+                <input type="username" value={inputValue1} onChange={handleChange1} placeholder="Username" className={styles.inputLogin}></input>
             </div>
             <div className={styles.bodyDivLogin2}>
-                <input type="password" placeholder="Password" className={styles.inputLogin}></input>
+                <input type="password" value={inputValue2} onChange={handleChange2} placeholder="Password" className={styles.inputLogin}></input>
             </div>
             <div className={styles.LoginButtonDiv}>
                 <Link href="/home">
-                    <button className={styles.buttonLogin}>Login</button>
+                    <button disabled={!buttonEnable} className={styles.buttonLogin}>Login</button>
                 </Link>
             </div>
             <div className={styles.LoginToSignupDiv1}>Don't Have an Account?
--- CoMMSFrontend/src/app/signup/page.tsx
+++ CoMMSFrontend/src/app/signup/page.tsx
@@ -1,27 +1,86 @@
+'use client'
+
 import Link from "next/link";
 import styles from "./page.module.css";
+import React, { useState } from 'react';
 
-export default function Jobs() {
+
+export default function Signup() {
+
+    const user = ref<Signup>()
+
+    const [inputValue1, setInputValue1] = useState('');
+    const [isValid1, setIsValid1] = useState(true)
+  
+    const handleChange1 = (event) => {
+      const value = event.target.value
+      setInputValue1(value);
+  
+      const isAlphaNumeric = /^[a-zA-Z0-9@.]+$/.test(value);
+      setIsValid1(isAlphaNumeric);
+    };
+
+    const [inputValue2, setInputValue2] = useState('');
+    const [isValid2, setIsValid2] = useState(true)
+  
+    const handleChange2 = (event) => {
+      const value = event.target.value
+      setInputValue2(value);
+  
+      const isNumeric = /^[0-9]+$/.test(value);
+      setIsValid2(isNumeric);
+    };
+
+  const [inputValue3, setInputValue3] = useState('');
+  const [isValid3, setIsValid3] = useState(true)
+
+  const handleChange3 = (event) => {
+    const value = event.target.value
+    setInputValue3(value);
+
+    const isAlphaNumeric = /^[a-zA-Z0-9]+$/.test(value);
+    setIsValid3(isAlphaNumeric);
+  };
+
+  const [inputValue4, setInputValue4] = useState('');
+
+  const handleChange4 = (event) => {
+    setInputValue4(event.target.value);
+  };
+
+  const [inputValue5, setInputValue5] = useState('');
+
+  const handleChange5 = (event) => {
+    setInputValue5(event.target.value);
+  };
+
+  const buttonEnable = inputValue1.length > 0 && inputValue2.length > 0 && inputValue3.length > 0 
+  && inputValue4.length > 0 && inputValue5.length > 0 && isValid1 && isValid2 && isValid3 
+  && inputValue4 === inputValue5;
+
   return (
     <main className={styles.main}>
         <div className={styles.headerDivLogin1}>Sign Up
         </div>
         <div className={styles.bodyDivLogin1}>
             <div className={styles.bodyDivLogin2}>
-                <input type="email" placeholder="Email" className={styles.inputLogin}></input>
+                <input type="email" value={inputValue1} onChange={handleChange1} placeholder="Email" className={styles.inputLogin}></input>
             </div>
             <div className={styles.bodyDivLogin2}>
-                <input type="phoneNumber" placeholder="Phone Number" className={styles.inputLogin}></input>
+                <input type="phoneNumber" value={inputValue2} onChange={handleChange2} placeholder="Phone Number" className={styles.inputLogin}></input>
             </div>
             <div className={styles.bodyDivLogin2}>
-                <input type="username" placeholder="Username" className={styles.inputLogin}></input>
+                <input type="username" value={inputValue3} onChange={handleChange3} placeholder="Username" className={styles.inputLogin}></input>
             </div>
             <div className={styles.bodyDivLogin2}>
-                <input type="password" placeholder="Password" className={styles.inputLogin}></input>
+                <input type="password" value={inputValue4} onChange={handleChange4} placeholder="Password" className={styles.inputLogin}></input>
+            </div>
+            <div className={styles.bodyDivLogin2}>
+                <input type="password" value={inputValue5} onChange={handleChange5} placeholder="Confirm Password" className={styles.inputLogin}></input>
             </div>
             <div className={styles.LoginButtonDiv}>
                 <Link href="/home">
-                    <button className={styles.buttonLogin}>Sign Up</button>
+                    <button disabled={!buttonEnable} className={styles.buttonLogin}>Sign Up</button>
                 </Link>
             </div>
             <div className={styles.LoginToSignupDiv1}>Already Have an Account?
