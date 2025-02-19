df84eba91c6a277b2f745fa1afafeca98a3120c3
Gunth15 <cjw041@email.latech.edu>
2025-02-18 21:49:44 -0600

    Organized files and added button to post materials

---
+++ frontend/components/add-material-form/material-add-dialog.tsx
@@ -0,0 +1,30 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription
+} from "@/components/ui/dialog"
+import React from "react";
+import MaterialForm from "./material-add-form";
+
+
+export default function AddMaterialFormDialouge({ children }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {children}
+      </DialogTrigger>
+      <DialogContent className="sm:mx-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add New Material</DialogTitle>
+          <DialogDescription>Add material to be managed. Once your done, everyone in who needs to see it can see it.</DialogDescription>
+        </DialogHeader>
+        <div>
+          <MaterialForm />
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
