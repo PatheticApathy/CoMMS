41d29ea545739d11d0acfea4a1997807962d516b
Gunth15 <cjw041@email.latech.edu>
2025-02-20 19:38:09 -0600

    Uploaded material api docs and finishes material table with options to add material and delete material

--- frontend/app/dashboard/material/jobsite/[id]/page.tsx
+++ frontend/app/dashboard/material/jobsite/[id]/page.tsx
@@ -1,7 +1,5 @@
 'use client'
-import { useState } from 'react'
 import { Material } from '@/material-api-types';
-import { Filter } from '@/components/table-material/materials-filter';
 import useSWR, { Fetcher } from 'swr'
 import Loading from '@/components/loading';
 import FilterAndTable from '@/components/table-material/material-filter+table';
@@ -10,22 +8,13 @@ import { useParams } from 'next/navigation';
 const fetcher: Fetcher<Material[], string> = async (...args) => fetch(...args).then(res => res.json())
 export default function JobSiteMaterialPage() {
 
-  const options: Filter = {
-    id: "",
-    quantity: "",
-    status: "",
-    type: "",
-    jobsite: "",
-  };
-
   const { id } = useParams()
   const { data: materials, error, isLoading } = useSWR(`/api/material/material/search?site=${id}`, fetcher)
-  const [filter, setFilter] = useState<Filter>(options);
 
 
   if (isLoading) { return (<div className='flex items-center justify-center w-screen h-screen'>Loading <Loading /></div>) }
   if (error) { return (<p className='flex items-center justify-center w-screen h-screen'>Error occured lol</p>) }
   return (
-    <FilterAndTable filter={filter} setFilterAction={setFilter} materials={materials} />
+    <FilterAndTable header={`Jobsite ${id} Material`} materials={materials} />
   );
 }
--- frontend/components/table-material/material-popup.tsx
+++ frontend/components/table-material/material-popup.tsx
@@ -11,8 +11,8 @@ import Image from "next/image";
 import useSWR, { Fetcher } from "swr"
 import Loading from "@/components/loading";
 
-const MaterialLogFetcher: Fetcher<MaterialLog[], string> = async (...args) => fetch(...args).then(res => res.json())
-const CheckoutLogFetcher: Fetcher<CheckoutLog[], string> = async (...args) => fetch(...args).then(res => res.json())
+const MaterialLogFetcher: Fetcher<MaterialLog[], string> = async (...args) => fetch(...args, { cache: 'force-cache' }).then(res => res.json())
+const CheckoutLogFetcher: Fetcher<CheckoutLog[], string> = async (...args) => fetch(...args, { cache: 'force-cache' }).then(res => res.json())
 
 const DisplayMaterialLogs = (material_logs: MaterialLog[] | undefined, error: Boolean, isLoading: Boolean,) => {
   if (isLoading) {
@@ -67,7 +67,7 @@ const DisplayCheckouts = (checkout_logs: CheckoutLog[] | undefined, error: Boole
         {
           checkout_logs.map((log) => {
             return (
-              <>
+              <div>
                 <h3>Chekout pertains to {log.user_id}</h3>
                 <br />
                 {(() => {
@@ -86,10 +86,12 @@ const DisplayCheckouts = (checkout_logs: CheckoutLog[] | undefined, error: Boole
                       return (
                         `Checked in on : ${checkin.toLocaleDateString()} ${checkin.toLocaleTimeString()}`
                       );
+                    } else {
+                      return ("Still checked out")
                     }
                   })()
                 }
-              </>
+              </div>
             )
           }
           )
@@ -100,57 +102,58 @@ const DisplayCheckouts = (checkout_logs: CheckoutLog[] | undefined, error: Boole
     return <div className="text-center">Never Checked out</div>;
   }
 }
-export default function MaterialSheet({ material, children, }: Readonly<{
+export default function MaterialSheet({ material, children }: Readonly<{
   material: Material,
   children: React.ReactNode;
 }>) {
 
-  const { data: material_logs, isLoading: material_loading, error: material_error } = useSWR(`/api/material/mlogs/recent?id=${material.id}`, MaterialLogFetcher)
   const { data: checkout_logs, isLoading: checkout_loading, error: checkout_error } = useSWR(`/api/material/checkout/recent?id=${material.id}`, CheckoutLogFetcher)
+  const { data: material_logs, isLoading: material_loading, error: material_error } = useSWR(`/api/material/mlogs/recent?id=${material.id}`, MaterialLogFetcher)
 
 
-  console.log(checkout_logs)
-  console.log(material_logs)
   return (
     <Sheet>
-      <SheetTrigger>{children}</SheetTrigger>
-      <SheetContent> {(() => {
-        if (material) {
-          return (
-            <SheetHeader>
-              <SheetTitle className="text-2xl">{material.name.Valid ? material.name.String : "No Name Found"}</SheetTitle>
-              <SheetDescription className="flex flex-col gap-10 text-lg">
-                <div>
-                  <Image src={'/file.svg'} alt="No Picture of Item found" width={500} height={500}></Image>
-                </div>
-                <div >
-                  {material.type.Valid ? `Type: ${material.type.String}` : "No type found"}
-                  <br />
-                  {`${material.status} with ${material.quantity} ${material.unit}`}
-                  <br />
-                  {material.last_checked_out ? `last checked out on ${material.last_checked_out}` : "Never checked out"}
-                  <hr />
-                  <div className="place-content-center">
-                    {
-                      DisplayMaterialLogs(material_logs, material_error, material_loading)
-                    }
-                  </div>
-                  <hr />
-                  <div className="place-content-center">
-                    {
-                      DisplayCheckouts(checkout_logs, checkout_error, checkout_loading)
-                    }
-                  </div>
-                </div>
-              </SheetDescription>
-            </SheetHeader>
-          )
-        }
-        else {
-          return (<p className='flex items-center justify-center h-screen'>No additional data for material</p>)
-        }
-      }
-      )()}
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent side="right" className="flex flex-col">
+        <div className="overflow-auto">
+          {(() => {
+            if (material) {
+              return (
+                <SheetHeader className="flex-none border-b text-left">
+                  <SheetTitle className="text-2xl">{material.name.Valid ? material.name.String : "No Name Found"}</SheetTitle>
+                  <SheetDescription className="flex flex-col gap-10 text-lg">
+                    <div>
+                      <Image src={'/file.svg'} alt="No Picture of Item found" width={500} height={500}></Image>
+                    </div>
+                    <div >
+                      {material.type.Valid ? `Type: ${material.type.String}` : "No type found"}
+                      <br />
+                      {`${material.status} with ${material.quantity} ${material.unit}`}
+                      <br />
+                      {material.last_checked_out ? `last checked out on ${material.last_checked_out}` : "Never checked out"}
+                      <hr />
+                      <div className="place-content-center">
+                        {
+                          DisplayMaterialLogs(material_logs, material_error, material_loading)
+                        }
+                      </div>
+                      <hr />
+                      <div className="place-content-center">
+                        {
+                          DisplayCheckouts(checkout_logs, checkout_error, checkout_loading)
+                        }
+                      </div>
+                    </div>
+                  </SheetDescription>
+                </SheetHeader>
+              )
+            }
+            else {
+              return (<p className='flex items-center justify-center h-screen'>No additional data for material</p>)
+            }
+          }
+          )()}
+        </div>
       </SheetContent>
-    </Sheet>)
+    </Sheet >)
 }
