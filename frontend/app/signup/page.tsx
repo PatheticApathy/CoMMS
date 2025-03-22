e40fa37695dbd8141ef54797074d7c9b05d2f117
mjl036 <mjl036@email.latech.edu>
2025-03-22 16:09:44 -0500

    More color changes

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -2,7 +2,7 @@ import SignupForm from "@/components/signup-form"
 
 export default function Signup() {
   return (
-    <div className="dark:bg-gray-800 flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="font-bold font-sans text-5xl basis-1/6">Join Us at CoMMS</h1>
       <SignupForm />
     </div>
