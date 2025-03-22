ebf75ce7112e0f429f72e4ff3c922563fe463b37
mjl036 <mjl036@email.latech.edu>
2025-03-22 12:18:58 -0500

    Temporary for update

--- frontend/app/dashboard/admin/page.tsx
+++ frontend/app/dashboard/admin/page.tsx
@@ -4,9 +4,26 @@ import { useState } from 'react';
 import { UserTable } from '@/components/admin-table';
 import { MaterialTable } from '@/components/admin-material';
 import { Button } from '@/components/ui/button';
+import useSWR from 'swr'
+import { getToken } from '@components/localStorage'
+
+async function getProfileArgs(url: string, arg: {token: string}) {
+  return fetch(url, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({token: arg})
+  }).then(res => res.json())
+}
 
 export default function AdminPage() {
   const [showFirstTable, setShowFirstTable] = useState(true);
+
+  let token = getToken()
+  let id = 1
+
+  const { data: tokenData, error: error2 } = useSWR(['api/user/decrypt', token], ([url, token]) => getProfileArgs(url, token))
+  if (tokenData)
+      id = tokenData.id
 
   return (
     <div className="flex flex-col justify-center items-center w-screen">
