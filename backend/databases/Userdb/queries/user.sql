1656384306b803fdc0298dd7b4fa26144614e847
Gunth15 <cjw041@email.latech.edu>
2025-03-22 11:59:29 -0500

    Fixed search handler

--- backend/databases/Userdb/queries/user.sql
+++ backend/databases/Userdb/queries/user.sql
@@ -2,7 +2,7 @@
 SELECT id, username, password, firstname, lastname, company_id, jobsite_id, role, email, phone, profilepicture FROM Users;
 
 -- name: GetUser :one
-SELECT id, username, password, firstname, lastname, company_id, jobsite_id, role, email, phone, profilepicture FROM Users WHERE id = ?;
+SELECT id, username,firstname, lastname, company_id, jobsite_id, role, email, phone, profilepicture FROM Users WHERE id = ?;
 
 -- name: GetUserName :one
 SELECT id, username, password, firstname, lastname, company_id, jobsite_id, role, email, phone, profilepicture FROM Users WHERE username = ?;
