e20fe542171373fbec1bda26f63b01d4c2313518
mjl036 <mjl036@email.latech.edu>
2025-03-22 16:08:51 -0500

    Correct color

--- frontend/app/dashboard/jobsite/page.tsx
+++ frontend/app/dashboard/jobsite/page.tsx
@@ -25,7 +25,7 @@ export default function JobSitePage() {
   }
 
   return (
-    <div className="dark:bg-gray-800 flex flex-col justify-center items-center h-screen w-screen">
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
       <h1 className="font-bold font-sans text-5xl basis-1/4">Jobsite Info</h1>
       <div><JobsitePaths /></div>
       <div className="w-full h-[500px]"><JobsiteMapWrapper/></div>
