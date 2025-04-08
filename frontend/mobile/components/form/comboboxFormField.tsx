f2c8dbd245e7641000ecddba394ff7ab4f249372
Gunth15 <cjw041@email.latech.edu>
2025-04-08 12:02:22 -0500

    this

--- frontend/mobile/components/form/comboboxFormField.tsx
+++ frontend/mobile/components/form/comboboxFormField.tsx
@@ -1,6 +1,6 @@
 import { Key, useState } from "react";
-import { Control } from "react-hook-form";
-import { Modal } from "react-native";
+import { Control, Controller } from "react-hook-form";
+import { Button, Modal, Text } from "react-native";
 
 export function ComboboxFormField({ form_attr, default_label, options }: { form_attr: { name: string, control: Control<any> }, default_label: string, options: { label: string, value: Key }[] }) {
   const [modalVisible, setModalVisible] = useState(false);
@@ -8,6 +8,13 @@ export function ComboboxFormField({ form_attr, default_label, options }: { form_
     animationType="slide"
     transparent={true}
   >
+    <Text>{form_attr.name}</Text>
+    <Controller
+      control={form_attr.control}
+      render={options.map((option) => {
+        <Button />
+      })}
+    />
   </Modal>
 
 }
