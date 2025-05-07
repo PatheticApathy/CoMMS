145ece80504d492ff3e9d22342ca01d4966315c7
PatheticApathy <jasonleblanc03@icloud.com>
2025-05-06 20:02:29 -0500

    Make it only list your company

--- frontend/web/components/add-jobsite-form/jobsite-add-form.tsx
+++ frontend/web/components/add-jobsite-form/jobsite-add-form.tsx
@@ -9,9 +9,10 @@ import useSWRMutation from "swr/mutation";
 import { ComboboxFormField } from "@/components/form-maker/form-combobox";
 import FormInput from "../form-maker/form-input";
 import { toast } from "sonner";
-import { JobSite, AddJobSiteParams, Company } from "@/user-api-types";
-import { getToken } from "@/components/identity-provider";
+import { GetUserRow, JobSite, AddJobSiteParams, Company } from "@/user-api-types";
+import { getToken, IdentityContext } from "@/components/identity-provider";
 import AddJobsiteMap from "./jobsite-add-map";
+import { useContext } from "react";
 
 // Schema for form
 const AddJobsiteSchema = z.object({
@@ -25,6 +26,15 @@ const AddJobsiteSchema = z.object({
 // Fetcher
 const PostAddJobsite = async (url: string, { arg }: { arg: AddJobSiteParams }) => fetch(url, { headers: { 'Authorization': getToken() }, method: 'POST', body: JSON.stringify(arg) });
 
+const fetchUser = async  (url: string): Promise<GetUserRow[]> => {
+  const res = await fetch(url,
+    { headers: { 'authorization': getToken() || 'bruh' } }
+  )
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+};
 const fetchCompanies: Fetcher<Company[], string> = async (...args) => fetch(...args, { headers: { 'Authorization': getToken() } }).then((res) => res.json());
 
 export default function JobsiteForm() {
@@ -40,7 +50,10 @@ export default function JobsiteForm() {
     }
   });
 
-  const { data: companies, error: companiesError } = useSWR("/api/company/all", fetchCompanies);
+  const identity = useContext(IdentityContext)
+  
+  const { data: user } = useSWR<GetUserRow[], string>(identity ? `/api/user/search?id=${identity?.id}` : null, fetchUser);
+  const { data: companies, error: companiesError } = useSWR(user ? `/api/company/search?id=${user[0]?.company_id.Int64}` : null, fetchCompanies);
 
   const { trigger, isMutating } = useSWRMutation('/api/sites/add', PostAddJobsite, {
     onError(err) {
@@ -90,16 +103,18 @@ export default function JobsiteForm() {
     return <div>Loading companies...</div>;
   }
 
-  const companiesOptions = companies.map(companies => ({
-    label: companies.name,
-    value: companies.id
+  const companiesData = Array.isArray(companies) ? companies : companies ? [companies] : [];
+
+  const companiesOptions = companiesData.map(company => ({
+    label: company.name,
+    value: company.id
   }));
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(SendAddJobsiteRequest)}>
         {isMutating ? <Button variant={'ghost'}>Sending</Button> : <Button variant="yellow" type="submit">Add Jobsite</Button>}
-        <ComboboxFormField form_attr={{ name: "company_id", description: "All known companies", form: form }} default_label="Choose a company" options={companiesOptions} />
+        <ComboboxFormField form_attr={{ name: "company_id", description: "Your known companies", form: form }} default_label="Choose a company" options={companiesOptions} />
         <FormInput name="name" placeholder="Name" description="" form={form} />
         <div>
         <AddJobsiteMap form={form}/>
